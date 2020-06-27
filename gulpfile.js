4bee31e4a161ab6d9462ff244055982441712934
Rodrigo Graça <rodrigograca31@gmail.com>
2020-06-27 19:11:43 +0100

    fix: 🐛 gulp 4 support

--- gulpfile.js
+++ gulpfile.js
@@ -1,111 +1,176 @@
 // including plugins
-const gulp = require('gulp');
+const gulp = require("gulp");
 const htmlmin = require("gulp-html-minifier-terser");
-const runSequence = require('run-sequence');
-const cleanCSS = require('gulp-clean-css');
-const terser = require('gulp-terser');
-const clean = require('gulp-clean');
+const cleanCSS = require("gulp-clean-css");
+const terser = require("gulp-terser");
+const clean = require("gulp-clean");
 
-const injectPartials = require('gulp-inject-partials');
-const imagemin = require('gulp-imagemin');
-const sitemap = require('gulp-sitemap');
-const brotli = require('gulp-brotli');
-const deleteEmpty = require('delete-empty');
+const injectPartials = require("gulp-inject-partials");
+const imagemin = require("gulp-imagemin");
+const sitemap = require("gulp-sitemap");
+const brotli = require("gulp-brotli");
+const deleteEmpty = require("delete-empty");
+const srcset = require("gulp-srcset");
 
-const folder = './';
-const siteUrl = 'https://portfolio.rodrigograca.com/';
+const folder = "./";
+const siteUrl = "https://portfolio.rodrigograca.com/";
 
 // npm init
 // npm install gulp browser-sync gulp-minify-html gulp-minify-css gulp-uglify run-sequence gulp-clean gulp-inject-partials gulp-sitemap gulp-imagemin --save
 
 // https://julienrenaux.fr/2014/05/25/introduction-to-gulp-js-with-practical-examples/
 
-gulp.task('default', ['maintask'], function (callback) {
-	//runs maintask first, then:
-	return callback;
+// gulp.task("images", () =>
+// 	gulp
+// 		.src("images/*.{jpg,png}")
+// 		.pipe(
+// 			srcset(
+// 				[
+// 					{
+// 						match: "(min-width: 3000px)",
+// 						width: [1920, 1280, 1024, 860, 540, 320],
+// 						format: ["jpg", "webp"],
+// 					},
+// 				],
+// 				{
+// 					skipOptimization: true,
+// 				}
+// 			)
+// 		)
+// 		.pipe(gulp.dest("dist"))
+// );
+
+gulp.task("clean", function () {
+	return gulp.src("./dist/", { read: false }).pipe(clean());
 });
 
-gulp.task('maintask', function(callback) {
-	// it runs 'clean' then 'copyall' then the other ones in parallel then callback
-	return runSequence(
-		'clean',
-		'copyall',
-		'sitemap',
-		['minify-html', 'minify-css', 'minify-js', 'minify-imgs'],
-        'brotli',
-		'clean-files',
-		'delete-empty',
-		callback);
+gulp.task("copyall", function () {
+	return gulp
+		.src([
+			folder + "**/*",
+			"!**/node_modules/**",
+			"!**/node_modules",
+			"!**dist/**",
+			"!**dist",
+			"!package.json",
+			"!package-lock.json",
+			"!gulpfile.js",
+			"!LICENSE.txt",
+			"!partials/**",
+		])
+		.pipe(gulp.dest("./dist/"));
 });
 
-
-gulp.task('clean', function () {
-    return gulp.src('./dist/', {read: false})
-        .pipe(clean());
+gulp.task("minify-html", () => {
+	return gulp
+		.src([
+			folder + "**/*.html",
+			"!**/node_modules/**",
+			"!**/node_modules",
+			"!**dist/**",
+			"!**dist",
+		])
+		.pipe(injectPartials())
+		.pipe(
+			htmlmin({
+				collapseWhitespace: true,
+				removeComments: true,
+				removeAttributeQuotes: true,
+				minifyJS: true,
+			})
+		)
+		.pipe(gulp.dest("./dist/"));
 });
 
-gulp.task('copyall', function () {
-	return gulp.src([folder + '**/*', '!**/node_modules/**', '!**/node_modules', '!**dist/**', '!**dist'])
-		.pipe(gulp.dest('./dist/'));
-});
-
-gulp.task('minify-html', () => {
-	return gulp.src([folder + '**/*.html', '!**/node_modules/**', '!**/node_modules', '!**dist/**', '!**dist'])
-    .pipe(injectPartials())
-	.pipe(htmlmin({
-		collapseWhitespace: true,
-		removeComments: true,
-		removeAttributeQuotes: true,
-		minifyJS: true
-	}))
-	.pipe(gulp.dest('./dist/'));
-});
-
-gulp.task('minify-css', function () {
-	return gulp.src([folder + '**/*.css', '!**/node_modules/**', '!**/node_modules', '!**dist/**', '!**dist'])
+gulp.task("minify-css", function () {
+	return gulp
+		.src([
+			folder + "**/*.css",
+			"!**/node_modules/**",
+			"!**/node_modules",
+			"!**dist/**",
+			"!**dist",
+		])
 		.pipe(cleanCSS())
-		.pipe(gulp.dest('./dist/'));
+		.pipe(gulp.dest("./dist/"));
 });
 
-gulp.task('minify-js', function () {
-	return gulp.src([folder + '**/*.js', '!**/node_modules/**', '!**/node_modules', '!**dist/**', '!**dist'])
-		.pipe(terser({
-			ecma: 6,
-			mangle: {
-				toplevel: true,
-			}
-		}))
-		.pipe(gulp.dest('./dist/'));
+gulp.task("minify-js", function () {
+	return gulp
+		.src([
+			folder + "**/*.js",
+			"!**/node_modules/**",
+			"!**/node_modules",
+			"!**dist/**",
+			"!**dist",
+		])
+		.pipe(
+			terser({
+				ecma: 6,
+				mangle: {
+					toplevel: true,
+				},
+			})
+		)
+		.pipe(gulp.dest("./dist/"));
 });
 
-gulp.task('minify-imgs', function () {
-	return gulp.src([folder + '**/*.{png,jpg,gif,svg,jpeg}', '!**/node_modules/**', '!**/node_modules', '!**dist/**', '!**dist'])
+gulp.task("minify-imgs", function () {
+	return gulp
+		.src([
+			folder + "**/*.{png,jpg,gif,svg,jpeg}",
+			"!**/node_modules/**",
+			"!**/node_modules",
+			"!**dist/**",
+			"!**dist",
+		])
 		.pipe(imagemin())
-		.pipe(gulp.dest('./dist/'));
+		.pipe(gulp.dest("./dist/"));
 });
 
-gulp.task('sitemap', function () {
-	gulp.src('index.html', {
-		read: false
-	})
-	.pipe(sitemap({
-		siteUrl: siteUrl
-	}))
-	.pipe(gulp.dest('./dist/'));
+gulp.task("sitemap", async function () {
+	gulp
+		.src(["index.html", "./project/**/**.html"], {
+			read: false,
+		})
+		.pipe(
+			await sitemap({
+				siteUrl: siteUrl,
+			})
+		)
+		.pipe(gulp.dest("./dist/"));
 });
 
 /* https://css-tricks.com/brotli-static-compression/ */
-gulp.task('brotli', function() {
-  return gulp.src("./dist/" + "**/*.{html,js,css,svg,json}")
-    .pipe(brotli.compress())
-    .pipe(gulp.dest('./dist/'));
+gulp.task("brotli", function () {
+	return gulp
+		.src("./dist/" + "**/*.{html,js,css,svg,json}")
+		.pipe(brotli.compress())
+		.pipe(gulp.dest("./dist/"));
 });
 
-gulp.task('clean-files', function () {
-    return gulp.src('./dist/**/*.{less,scss,map,DS_Store}', {read: false})
-        .pipe(clean());
+gulp.task("clean-files", function () {
+	return gulp
+		.src(["./dist/**/*.{less,scss,map,DS_Store}", "./dist/partials/**"], {
+			read: false,
+		})
+		.pipe(clean());
 });
 
-gulp.task('delete-empty', function () {
-	return deleteEmpty('./dist/');
+gulp.task("delete-empty", function () {
+	return deleteEmpty("./dist/");
 });
+
+gulp.task(
+	"default",
+	gulp.series(
+		"clean",
+		"copyall",
+		// "images",
+		"sitemap",
+		gulp.parallel("minify-html", "minify-css", "minify-js", "minify-imgs"),
+		"brotli",
+		"clean-files",
+		"delete-empty"
+	)
+);
